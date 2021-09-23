c33015c31365463aaccf6e35a83bad4e1085e90e
SudPY <91254009+SudPY@users.noreply.github.com>
2021-09-23 14:56:15 +0530

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,15 @@
+print("Welcome to Simple Calculator!")
+x = 5
+y = 15
+
+sum = x + y
+difference = x - y
+product = x * y
+division = y/x 
+remainder = y%x
+
+print("sum of x and y = ", sum)
+print("difference of x and y = ", difference)
+print("product of x and y = ", product)
+print("quotient of x / y = ", division)
+print("remainder of x / y = ", remainder)
